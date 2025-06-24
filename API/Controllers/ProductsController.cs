4449ae18ebaf73622f910305c64d0b8695a7341d
baobao0303 <baobao22.work@gmail.com>
2025-06-24 19:17:39 +0700

    refactor: move data access layer to Infrastructure project
    Restructure project architecture by moving data access components from API to Infrastructure layer. This includes:
    - Moving StoreContext and related entities
    - Adding Entity Framework Core dependencies to Infrastructure
    - Updating API project references
    - Cleaning up generated files and migrations

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,5 @@
-using API.Data;
-using API.Entities;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +7,7 @@ namespace API.AddControllers
 {
     
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     public class ProductsController : ControllerBase
     {
         private readonly StoreContext _context;
