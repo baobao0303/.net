75e49cbde0dfebf62bf1d50c37835f0e8e23164f
Lê Tấn Bảo Bảo <110554959+baobao0303@users.noreply.github.com>
2025-06-26 17:14:25 +0700

    feat: initialize Angular client application with TailwindCSS
    Add base Angular application structure with routing, styling setup using TailwindCSS, and development configuration

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
